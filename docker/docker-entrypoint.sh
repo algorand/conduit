32e227e724a8039c3b3194f02e5e113abe26eb29
Will Winder <wwinder.unh@gmail.com>
2023-04-20 11:21:52 -0400

    Update docker/docker-entrypoint.sh
    Co-authored-by: Zeph Grunschlag <tzaffi@users.noreply.github.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # To allow mounting the data directory we need to change permissions
 # to our algorand user. The script is initially run as the root user
-# in order to change permissions, afterwards the script is re-launched
+# in order to change permissions; afterwards, the script is re-launched
 # as the algorand user.
 if [ "$(id -u)" = '0' ]; then
   chown -R algorand:algorand $CONDUIT_DATA_DIR
