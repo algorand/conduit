4e3d9a41fc2bfa99415399d4e0ef19068de1da91
Zeph Grunschlag <zeph@algorand.com>
2023-05-17 23:50:20 -0500

    consider go-sum as well + fix bug

--- go_mod_tidier.go
+++ go_mod_tidier.go
@@ -13,17 +13,24 @@ const maxTries = 10
 const goMod = "go.mod"
 
 func main() {
-	var prevModFile, nextModFile *modfile.File
+	nextModFile, nextSum, err := getAllAndTidyUp()
+	if err != nil {
+		panic(fmt.Sprintf("problem with go_mod_tidier round 0: %v", err))
+	}
+	var prevModFile *modfile.File
+	var prevSum string
 	var tryNum int
-	for tryNum = 1; tryNum == 1 || tryNum <= maxTries; tryNum++ {
-		if reflect.DeepEqual(prevModFile, nextModFile) {
-			break
-		}
+	for tryNum = 1; tryNum <= maxTries; tryNum++ {
+		fmt.Printf("go_mod_tidier round %d\n", tryNum)
 		prevModFile = nextModFile
+		prevSum = nextSum
 		var err error
-		nextModFile, err = getAllAndTidyUp()
+		nextModFile, nextSum, err = getAllAndTidyUp()
 		if err != nil {
 			panic(fmt.Sprintf("problem with go_mod_tidier round %d: %v", tryNum, err))
+		}
+		if reflect.DeepEqual(prevModFile, nextModFile) && prevSum == nextSum {
+			break
 		}
 	}
 	if tryNum > maxTries {
@@ -32,15 +39,21 @@ func main() {
 	fmt.Printf("Success! go_mod_tidier took %d tries to obtain a consistent result\n", tryNum-1)
 }
 
-func getAllAndTidyUp() (*modfile.File, error) {
+func getAllAndTidyUp() (*modfile.File, string, error) {
 	goModBytes, err := ioutil.ReadFile(goMod)
 	if err != nil {
-		return nil, fmt.Errorf("problem reading %s: %w", goMod, err)
+		return nil, "", fmt.Errorf("problem reading %s: %w", goMod, err)
 	}
+
+	goSumBytes, err := ioutil.ReadFile("go.sum")
+	if err != nil {
+		return nil, "", fmt.Errorf("problem reading go.sum: %w", err)
+	}
+	sum := string(goSumBytes)
 
 	mod, err := modfile.Parse(goMod, goModBytes, nil)
 	if err != nil {
-		return mod, fmt.Errorf("problem parsing %s: %w", goMod, err)
+		return mod, sum, fmt.Errorf("problem parsing %s: %w", goMod, err)
 	}
 
 	direct := make([]string, 0)
@@ -55,15 +68,15 @@ func getAllAndTidyUp() (*modfile.File, error) {
 		cmd := exec.Command("go", "get", pinnedPkg)
 		err := cmd.Run()
 		if err != nil {
-			return mod, fmt.Errorf("problem with: go get %s: %w", pinnedPkg, err)
+			return mod, sum, fmt.Errorf("problem with: go get %s: %w", pinnedPkg, err)
 		}
 	}
 
 	cmdA := exec.Command("go", "mod", "tidy", fmt.Sprintf("-compat=%s", mod.Go.Version))
 	err = cmdA.Run()
 	if err != nil {
-		return mod, fmt.Errorf("problem with: go mod tidy: %w", err)
+		return mod, sum, fmt.Errorf("problem with: go mod tidy: %w", err)
 	}
 
-	return mod, nil
+	return mod, sum, nil
 }
