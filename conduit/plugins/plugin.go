b96486c02e842ce234bf70222cb258a93157ae0b
Zeph Grunschlag <zeph@algorand.com>
2023-05-28 13:03:12 -0500

    enhancement: plugin interface

---
+++ conduit/plugins/plugin.go
@@ -0,0 +1,15 @@
+package plugins
+
+type Plugin interface {
+	// PluginMetadata - implement this interface.
+	PluginMetadata
+
+	// Config returns the configuration options used to create a Plugin.
+	// Initialized during Init, it should return nil until the Exporter has been Connected.
+	Config() string
+
+	// Close will be called during termination of the Conduit process.
+	// There is no guarantee that plugin lifecycle hooks will be invoked in any specific order in relation to one another.
+	// Returns an error if it fails which will be surfaced in the logs, but the process is already terminating.
+	Close() error
+}
--- conduit/plugins/processors/processor.go
+++ conduit/plugins/processors/processor.go
@@ -3,19 +3,15 @@ package processors
 import (
 	"context"
 
-	"github.com/sirupsen/logrus"
-
 	"github.com/algorand/conduit/conduit/data"
 	"github.com/algorand/conduit/conduit/plugins"
+	"github.com/sirupsen/logrus"
 )
 
 // Processor an interface that defines an object that can filter and process transactions
 type Processor interface {
-	// PluginMetadata implement this interface.
-	plugins.PluginMetadata
-
-	// Config returns the configuration options used to create the Processor.
-	Config() string
+	// Plugin - implement this interface.
+	plugins.Plugin
 
 	// Init will be called during initialization, before block data starts going through the pipeline.
 	// Typically, used for things like initializing network connections.
@@ -23,9 +19,7 @@ type Processor interface {
 	// The Config passed to Init() will contain the unmarshalled config file specific to this plugin.
 	Init(ctx context.Context, initProvider data.InitProvider, cfg plugins.PluginConfig, logger *logrus.Logger) error
 
-	// Close will be called during termination of the Indexer process.
-	Close() error
-
-	// Process will be called with provided optional inputs.  It is up to the plugin to check that required inputs are provided.
+	// Process will be called with provided optional inputs.
+	// It is up to the plugin to check that required inputs are provided.
 	Process(input data.BlockData) (data.BlockData, error)
 }
